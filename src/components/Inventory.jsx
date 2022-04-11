1c48e08c63bb04fd7e215e0173e4dbd62eb2f3d2
sivagurubilli <gurusrija123@gmail.com>
2022-04-11 07:35:53 +0530

    inventory

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,19 +1,38 @@
 import { useState } from "react";
 
+
+
 export const Inventory = () => {
-  const [inv, setInv] = useState({
+const [booksCount,setBooksCount] = useState(10);
+const [notebooksCount,setnotebooksCount] = useState(13)
+const [pensCount,setpensCount] = useState(40)
+const [InkpensCount,setInkpensCount]=useState(20)
+  
+  /*const [inv, setInv] = useState({
     books: 10,
     notebooks: 13,
     pens: 40,
-  });
+    Inkpens:20
+  });*/
     // Create add and remove functions here that changes the
     // state.
 
+    const handleChange=(option,value)=>{
+      if (option ==="books"&& booksCount>=0){
+        setBooksCount(booksCount+value)
+      }else if(option ==="notebooks"&& notebooksCount>=0){
+        setnotebooksCount(notebooksCount+value)
+      }else if(option ==="pens"&& pensCount>=0){
+        setpensCount(pensCount+value)
+      }else if(option  ===  "Inkpens"&& InkpensCount>=0){
+        setInkpensCount(InkpensCount+value)
+      }
+      
+    }
+    
 
-    let totalval = inv.books+inv.notebooks+inv.pens
-let handle =(val,prt)=>{
-  if(prt =="books")
-}
+
+ 
 
   return (
     <div
@@ -28,30 +47,30 @@ let handle =(val,prt)=>{
     >
       <div className="items">
         <span>Books: </span>
-        <button className="circlularButton">+</button>
-        <button className="circlularButton">-</button>
-        <span>{inv.books}</span>
+        <button onClick ={()=>handleChange("books",1)} className="circlularButton">+</button>
+        <button onClick ={()=>handleChange("books",-1)} className="circlularButton">-</button>
+        <span>{booksCount<0 ? setBooksCount(0):booksCount}</span>
       </div>
       <div className="items">
         <span>Notebooks: </span>
-        <button className="circlularButton">+</button>
-        <button className="circlularButton">-</button>
-        <span>{inv.notebooks}</span>
+        <button onClick ={()=>handleChange("notebooks",1)} className="circlularButton">+</button>
+        <button onClick ={()=>handleChange("notebooks",-1)} className="circlularButton">-</button>
+        <span>{notebooksCount<0 ? setnotebooksCount(0):notebooksCount}</span>
       </div>
       <div className="items">
         <span>Pen: </span>
-        <button className="circlularButton">+</button>
-        <button className="circlularButton">-</button>
-        <span>{inv.pens}</span>
+        <button onClick ={()=>handleChange("pens",1)} className="circlularButton">+</button>
+        <button  onClick ={()=>handleChange("pens",-1)} className="circlularButton">-</button>
+        <span>{pensCount<0 ? setpensCount(0):pensCount}</span>
       </div>
       <div className="items">
         <span>Ink Pens: </span>
-        <button className="circlularButton">+</button>
-        <button className="circlularButton">-</button>
-        <span>{inv.inkpens}</span>
+        <button onClick ={()=>handleChange("Inkpens",1)} className="circlularButton">+</button>
+        <button onClick ={()=>handleChange("Inkpens",-1)} className="circlularButton">-</button>
+        <span>{InkpensCount<0 ? setInkpensCount(0):InkpensCount}</span>
       </div>
             {/*calculate total and show it*/}
-      total: {0}
+      total: {" "}{booksCount+notebooksCount+pensCount<0?0 :booksCount+notebooksCount+pensCount+InkpensCount}
     </div>
   );
 };
